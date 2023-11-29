59b34c6f63aad40cb0f693e822183a43f4858e97
Michael Love <michael.love@issuerdirect.com>
2023-11-29 15:14:17 -0500

    implement the menu method.

--- src/NovaPasswordReset.php
+++ src/NovaPasswordReset.php
@@ -16,4 +16,19 @@ class NovaPasswordReset extends Tool
     {
         Nova::script('nova-password-reset', __DIR__.'/../dist/js/tool.js');
     }
+
+    /**
+     * Build the menu that renders the navigation links for the tool.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return mixed
+     */
+    public function menu(Request $request)
+    {
+//        return MenuSection::make('Beat Mapping')
+//            ->path('/tool-beat-topic-mapping')
+//            ->icon('map');
+
+        return [];
+    }
 }
